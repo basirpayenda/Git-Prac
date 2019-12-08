90965093b0da895c02afc53aaedcee7f53c48d9f
basirpayenda <ab.basir.payenda@gmail.com>
2019-12-09 00:32:47 +0500

    File added

---
+++ calc.py
@@ -0,0 +1,12 @@
+class Calc:
+    def add(self, a, b):
+        pass
+
+    def subtract(self, a, b):
+        pass
+
+    def multiply(self, a, b):
+        pass
+
+    def divide(self, a, b):
+        pass
